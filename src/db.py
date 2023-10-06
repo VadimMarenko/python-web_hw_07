466287d9c660526ed8478b891f4f60dfd92f1571
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-10-06 10:16:19 +0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -5,6 +5,7 @@ import configparser
 import pathlib
 
 
+# file_config = pathlib.Path.home().joinpath("config.ini")
 file_config = pathlib.Path(__file__).parent.parent.joinpath("config.ini")
 config = configparser.ConfigParser()
 config.read(file_config)
